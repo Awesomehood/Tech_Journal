7428d730434dc2dbe97f58b068e22d7f38a43e54
awesomehood <william.atwood017@gmail.com>
2020-02-24 02:10:18 -0500

    ssh script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -6,7 +6,7 @@
 user=$1
 sudo useradd -m -d /home/$user -s /bin/bash $user
 sudo mkdir /home/$user/.ssh
-sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sudo cp /home/william/Tech_Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
 sudo chmod 700 /home/$user/.ssh
-sudo chmod 600 /home/$user/sys265/.ssh/authorized_keys
+sudo chmod 600 /home/$user/.ssh/authorized_keys
 sudo chown -R $user:$user /home/$user/.ssh
