90069a2ef9150f9bf0f22b602b539cf89e298f60
awesomehood <william.atwood017@gmail.com>
2020-02-24 00:45:33 -0500

    code shell

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author william
+#creates a new user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
