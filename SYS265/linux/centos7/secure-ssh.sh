498937d43f233fc0811e3ac8061beb85ee47d2ec
awesomehood <william.atwood017@gmail.com>
2020-02-24 01:38:52 -0500

    ssh script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,10 @@
 #creates a new user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+user=$1
+sudo useradd -m -d /home/$user -s /bin/bash $user
+sudo mkdir /home/$user/.ssh
+sudo cp /home/william/Tech_Journal/SYS265/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/sys265/.ssh/authorized_keys
+sudo chown -R $user:$user /home/$user/.ssh
